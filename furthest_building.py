9a3017707919fd5690a14f4abfb774effccb67a1
salitayu <86502199+salitayu@users.noreply.github.com>
2022-06-21 21:00:21 -0400

    more test cases

--- furthest_building.py
+++ furthest_building.py
@@ -1,4 +1,3 @@
-import heapq
 '''
 You are given int array heights representing heights of buildings,
 some bricks, and some ladders.
@@ -19,6 +18,9 @@ Explanation: Starting at building 0, you can follow these steps:
 - Go to building 4 using your only ladder. You must use either bricks or ladders because 6 < 9.
 It is impossible to go beyond building 4 because you do not have any more bricks or ladders.
 '''
+
+import heapq
+
 def furthest_building(heights, bricks, ladders):
     p = []
     n = len(heights)
@@ -32,4 +34,7 @@ def furthest_building(heights, bricks, ladders):
             if bricksUsed > bricks:
                 return i - 1
     return n - 1
-    
+
+print(furthest_building([4,2,7,6,9,14,12],5,1))
+print(furthest_building([4,12,2,7,3,18,20,3,19],10,2))
+print(furthest_building([14,3,19,3],17,0))
