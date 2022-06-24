6416eba53d3c952417f2b0851c763adbd20fc10f
salitayu <86502199+salitayu@users.noreply.github.com>
2022-06-23 20:34:32 -0400

    quickselect algorithm

--- kth_largest.py
+++ kth_largest.py
@@ -23,13 +23,26 @@ def findKthLargest2(nums, k):
     return p[0]
 
 # quickselect algorithm
-def partition(nums):
-    pivot = nums[-1]
-    p += 1
+def findKthLargest3(nums, k):
+    k = len(nums) - k
+    def quickSelect(l,r):
+        pivot, p = nums[r], l
+        for i in range(l, r):
+            if nums[i] <= pivot:
+                nums[p], nums[i] = nums[i], nums[p]
+                p += 1
+        nums[p], nums[r] = nums[r], nums[p]
+        if p > k:
+            return quickSelect(l, p - 1)
+        elif p < k:
+            return quickSelect(p+1, r)
+        else:
+            return nums[p]
+    return quickSelect(0, len(nums) - 1)
 
 nums = [3,2,1,5,6,4]
 k = 2
-print(findKthLargest2(nums, k))
+print(findKthLargest3(nums, k))
 nums = [3,2,3,1,2,4,5,5,6]
 k = 4
-print(findKthLargest2(nums, k))
+print(findKthLargest3(nums, k))
