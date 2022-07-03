ce20e615a18f958d33db8ab84a348511fec02d42
salitayu <86502199+salitayu@users.noreply.github.com>
2022-07-03 16:08:26 -0400

    wiggle subsequence

---
+++ wiggle_subsequence.py
@@ -0,0 +1,17 @@
+def wiggleMaxLength(nums):
+    if len(nums) < 2:
+        return len(nums)
+    previousdiff = nums[1] - nums[0]
+    count = 2 if nums[1] - nums[0] > 0 else 1
+    for i in range(2, len(nums)):
+        currentdiff = nums[i] - nums[i-1]
+        if (currentdiff > 0 and previousdiff < 0) or (currentdiff < 0 and previousdiff > 0):
+            count += 1
+            previousdiff = currentdiff
+    return count
+nums = [1,7,4,9,2,5]
+print(wiggleMaxLength(nums)) # 6
+nums = [1,17,5,10,13,15,10,5,16,8]
+print(wiggleMaxLength(nums)) # 7
+nums = [1,2,3,4,5,6,7,8,9]
+print(wiggleMaxLength(nums)) # 2
