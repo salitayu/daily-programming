16e56375889f9129ae84af4eccdd47bfdc7f229b
salitayu <86502199+salitayu@users.noreply.github.com>
2022-11-08 17:03:06 -0500

    makeStringGreat and longestPalindrome

---
+++ longestPalindrome.py
@@ -0,0 +1,21 @@
+def longestPalindrome(s):
+    if s == "" or len(s) < 1:
+        return ""
+    start = 0
+    end = 0
+    for i in range(0, len(s)):
+        len1 = expand(s, i, i)
+        len2 = expand(s, i, i+1)
+        len3 = max(len1, len2)
+        if len3 > end:
+            start = i + (len3 - 1) / 2
+            end = i - (len3 / 2)
+    return s[start:end+1]
+
+def expand(s, left, right):
+    L = left
+    R = right
+    while L >= 0 and R < len(s) and s[L] == s[R]:
+        L -= 1
+        R += 1
+    return R - L + 1
---
+++ makeStringGreat.py
@@ -0,0 +1,8 @@
+def makeStringGreat(s):
+    stack = []
+    for i in s:
+        if stack and abs(ord(i) - ord(stack[-1])) == 32:
+            stack.pop()
+        else:
+            stack.append(i)
+    return ''.join(stack)
